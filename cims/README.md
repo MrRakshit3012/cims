a4dd07c2f00bc1641f67fd2652051b96e864aaf7
Rakshit <1da20cs111.cs@drait.edu.in>
2023-03-16 00:28:49 +0530

    tyagi

---
+++ cims/README.md
@@ -0,0 +1,2 @@
+# cims
+we build coaching management system website using HTML ,  CSS , Java Script , Php , SQL. To see the website type http://localhost/cims /
---
+++ modules/admin/images/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 6/27/2018 this is the usual date
+ * Time: 9:13 AM
+ */
+
+header("Location: ../");
